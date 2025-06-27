365752d680fccd73c628f903476ed68e0f9df4a1
Samuel Lopes <samuelvitorprofissional@gmail.com>
2025-06-27 01:45:07 -0300

    pratica 2 - startando

---
+++ app/src/main/java/com/weatherapp/ui/HomePage.kt
@@ -0,0 +1,46 @@
+package com.weatherapp.ui
+
+import android.app.Activity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun HomePage(modifier: Modifier = Modifier) {
+    val activity = LocalContext.current as? Activity
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Bem-vindo/a!",
+            fontSize = 24.sp
+        )
+        Spacer(modifier = Modifier.size(24.dp))
+        Row(modifier = modifier) {
+            Button(
+                onClick = {
+                    activity?.finish()
+                }
+            ) {
+                Text("Sair")
+            }
+        }
+    }
+}
